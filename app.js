a903c02b245ed04b92f011554e5dea0caf9106ee
Anthony Kelani <ankela@microsoft.com>
2016-12-06 14:26:07 -0800

    Set up Active Directory for login. TODO: Embed PowerBI report in dashboad.

--- app.js
+++ app.js
@@ -1,12 +1,91 @@
+'use strict';
 var express = require('express');
+var expressSession = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var bunyan = require('bunyan');
+var passport = require('passport');
+var config = require('./config');
+
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+
+// add a logger
+var log = bunyan.createLogger({
+    name: 'Microsoft OIDC Example Web Application'
+});
+
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier), and invoke a callback
+//   with a user object.
+passport.use(new OIDCStrategy({
+    identityMetadata: config.creds.identityMetadata,
+    clientID: config.creds.clientID,
+    responseType: config.creds.responseType,
+    responseMode: config.creds.responseMode,
+    redirectUrl: config.creds.redirectUrl,
+    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+    clientSecret: config.creds.clientSecret,
+    validateIssuer: config.creds.validateIssuer,
+    isB2C: config.creds.isB2C,
+    issuer: config.creds.issuer,
+    passReqToCallback: config.creds.passReqToCallback,
+    scope: config.creds.scope,
+    loggingLevel: config.creds.loggingLevel,
+    nonceLifetime: config.creds.nonceLifetime
+  },
+  function(iss, sub, profile, accessToken, refreshToken, done) {
+    if (!profile.oid) {
+      return done(new Error("No email found"), null);
+    }
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      findByOid(profile.oid, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          // "Auto-registration"
+          user_sessions.push(profile);
+          return done(null, profile);
+        }
+        return done(null, user);
+      });
+    });
+  }
+));
+
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.oid);
+});
+
+passport.deserializeUser(function(id, done) {
+  findByOid(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// array to hold logged in users
+var user_sessions = [];
+
+var findByOid = function(oid, fn) {
+  for (var i = 0, len = user_sessions.length; i < len; i++) {
+    var user = user_sessions[i];
+    if (user.oid === oid) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+};
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -20,11 +99,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({ secret: 'microsoft for startups', resave: true, saveUninitialized: false }))
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index(passport));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,4 +127,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+
+module.exports = app, passport;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(passport) {
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
 
-module.exports = router;
+  router.get('/dashboard', ensureAuthenticated, function(req, res, next) {
+    console.log(req.user.name.givenName);
+    res.render('dashboard', { title: 'Dashboard', user: req.user });
+  })
+
+  router.get('/auth/failure', function(req, res, next) {
+    res.status(err.status || 500);
+    res.render('error');
+  })
+
+  router.get('/auth/openid', 
+  passport.authenticate('azuread-openidconnect', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/auth/failure',
+    failureFlash : true
+  }),
+  function(req, res) {
+    console.log('Login Successful - /auth/openid');
+    res.redirect('/dashboard');
+  });
+
+  // 'GET returnURL'
+  // `passport.authenticate` will try to authenticate the content returned in
+  // query (such as authorization code). If authentication fails, user will be
+  // redirected to '/' (home page); otherwise, it passes to the next middleware.
+
+  router.get('/auth/openid/return',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' }),
+  function(req, res) {
+    console.log('Login Successful - GET /auth/openid/return');
+    res.redirect('/');
+  });
+
+  // 'POST returnURL'
+  // `passport.authenticate` will try to authenticate the content returned in
+  // body (such as authorization code). If authentication fails, user will be
+  // redirected to '/' (home page); otherwise, it passes to the next middleware.
+
+  router.post('/auth/openid/return',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' }),
+  function(req, res) {
+    console.log('Login Successful - POST /auth/openid/return');
+    res.redirect('/dashboard');
+  });
+
+  //   Use this route middleware on any resource that needs to be protected.  If
+  //   the request is authenticated (typically via a persistent login session),
+  //   the request will proceed.  Otherwise, the user will be redirected to the
+  //   login page.
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/')
+  }
+
+  return router;
+}
